261e23ba09973d0931e701e93a1b462d974c601d
刘英伟 <yingwei_liu_2022@163.com>
2022-09-24 23:32:47 +0800

    1

--- me_demo/深浅拷贝.py
+++ me_demo/深浅拷贝.py
@@ -26,12 +26,31 @@
 """
 深拷贝
 """
-import copy
+# import copy
+#
+# test_dict = {'key1': 'val1', 'key2': [1, 2, 3]}  # 可变容器
+# new_dict = copy.deepcopy(test_dict)
+# print("test_dict的内存地址:", id(test_dict))
+# print("new_dict的内存地址 :", id(new_dict))
+# test_dict['key2'].append(4)
+# print("test_dict:", test_dict)
+# print("new_dict:", new_dict)
 
-test_dict = {'key1': 'val1', 'key2': [1, 2, 3]}  # 可变容器
-new_dict = copy.deepcopy(test_dict)
-print("test_dict的内存地址:", id(test_dict))
-print("new_dict的内存地址 :", id(new_dict))
-test_dict['key2'].append(4)
-print("test_dict:", test_dict)
-print("new_dict:", new_dict)
+
+"""
+集合set
+"""
+# test_list = [1, 2, 2, 3, 3, 4, 4, 5]
+# print(set(test_list))
+# set1 = {7, 10, 3, 4, 5, 6, "aa"}
+# print(set1)
+# res = set1.pop()
+# print(res)
+# print(set1)
+test_list = [10, 1, 5, 4, 2, 9, 9, 9, 10, "test"]
+# print(test_list)
+# # list排序
+# test_list.sort()
+# print(test_list)
+# print("排序:", sorted(test_list))
+print(set(test_list))
