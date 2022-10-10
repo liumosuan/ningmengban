b9672a9bd579ad94801ba2791a02731c2cc71896
Lateluck <1437794866@qq.com>
2022-10-10 23:21:21 +0800

    updata10.10.4

--- day04/home_work.py
+++ day04/home_work.py
@@ -45,15 +45,16 @@ dict_info["phone"] = fk.phone_number()
 print(dict_info)
 
 # c, 平台为了保护你的隐私，需要你删除你的联系方式；
-del dict_info["phone"]
+dict_info.pop("phone")
+# del dict_info["phone"]
 print(dict_info)
 
-# d, 你为了取得更好的成绩，需要取一个花名，并修改自己的身高和其他你觉得需要改的信息。
-dict_info["nickname"] = "人生苦短，我用python"
-dict_info["height"] = 190
-dict_info["income"] = "30W/年"
-print(dict_info)
-
-# e, 你进一步添加自己的兴趣，兴趣至少包含 3个(注意：兴趣是另外一个列表。请将这个列表作为一个成员，添加到原个人信息列表当中，添加到末尾即可。
-dict_info["hobbies"] = ['学python自动化', "赚钱", "看美女"]
-print(dict_info)
+# # d, 你为了取得更好的成绩，需要取一个花名，并修改自己的身高和其他你觉得需要改的信息。
+# dict_info["nickname"] = "人生苦短，我用python"
+# dict_info["height"] = 190
+# dict_info["income"] = "30W/年"
+# print(dict_info)
+#
+# # e, 你进一步添加自己的兴趣，兴趣至少包含 3个(注意：兴趣是另外一个列表。请将这个列表作为一个成员，添加到原个人信息列表当中，添加到末尾即可。
+# dict_info["hobbies"] = ['学python自动化', "赚钱", "看美女"]
+# print(dict_info)
---
+++ day05_循环控制/if条件怎么写.py
@@ -0,0 +1,37 @@
+# 判断字符串是否为空
+# a = "123"
+# if a:
+#     print("a不为空")
+# else:
+#     print("a为空")
+#
+# print(bool(a))
+
+# 列表
+# a = []
+# print(bool(a))
+# if a:
+#     print("列表不为空")
+# else:
+#     print("列表为空")
+
+# 字典
+# a = {"key": "value"}
+# print(bool(a))
+# if a:
+#     print("字典不为空")
+# else:
+#     print("字典为空")
+
+# a = -1
+# if not a:  # if not 取反
+#     print("不为空")
+# else:
+#     print("为空")
+
+a = 1
+b = [1, 2, 3]
+if a in b:
+    print("yes")
+else:
+    print("no")
