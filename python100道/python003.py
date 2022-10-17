675b333fffb6b94055a84874fe324158640a4be8
Lateluck <1437794866@qq.com>
2022-10-17 23:16:30 +0800

    11

--- python100道/python003.py
+++ python100道/python003.py
@@ -1,3 +1,18 @@
 """
 题目003：一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
 """
+
+d=5
+def demo(x, **y):
+    global d    # 申明d为全局变量
+    d+=1
+    print(x)
+    print(y)
+
+
+# demo(1,a=2,b=3,c=5)
+#
+# def test(x,*y):
+#     print(x)
+#     print(y)
+# test(1,2,3,4)
